d180540c11bbd84cbfd1fbbfdbd9f8430d033ecd
RyanA3 <alsobrooks.ryan@gmail.com>
2022-11-06 10:13:57 -0800

    Fix Implementation

--- csc1500/ForestDB/src/ForestDatabase.py
+++ csc1500/ForestDB/src/ForestDatabase.py
@@ -14,9 +14,11 @@ template_field_types = [str, str, str, str, int, int, str];
 EMPLOYEE_TEMPLATE = AbstractDatabase.EntryTemplate(template_field_names, template_field_types);
 
 
+
 #Employee object builder
 Employee = lambda fields: AbstractDatabase.Entry(EMPLOYEE_TEMPLATE, fields);
 
 
-anemployee = Employee(['Ryan', 'Shift manager', '14935', 'albr@ww.com', 5865321314, 11029, 'Gaming']);
-print(anemployee);
+
+#Define database and load into memory
+DATABASE = AbstractDatabase.Database(EMPLOYEE_TEMPLATE);
